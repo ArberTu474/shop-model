126742371153ad6729143350c409fc2e081faf5d
ArberTu474 <arber.tusha@outlook.com>
2022-01-26 19:49:51 +0100

    Added bookmard functionality

--- script/main.js
+++ script/main.js
@@ -59,6 +59,20 @@ function openCart() {
   homeIcon.setAttribute("class", "material-icons-outlined nav-icon");
 }
 
+const bookmarkPopUP = document.querySelector("#bookmarkPopUp");
+
+function openBookmark() {
+  bookmarkPopUP.style.display = "flex";
+  html.style.height = "100%";
+  html.style.overflow = "hidden";
+  body.style.height = "100%";
+  body.style.overflow = "hidden";
+  // Remove styling form others
+  cartPill.setAttribute("class", "nav-icon-container cart-pill");
+  homePill.setAttribute("class", "nav-icon-container home-pill");
+  homeIcon.setAttribute("class", "material-icons-outlined nav-icon");
+}
+
 // checks if the total price is 0 and formats the cart pop up
 function cartUiUpdate() {
   if (totalPrice <= 0) {
@@ -100,13 +114,32 @@ function closeCart() {
   homeIcon.setAttribute("class", "material-icons nav-icon");
 }
 
-// OFFERS
-
 const bookmarkIcon = document.querySelector("#bookmarkIcon");
 const bookmarkPill = document.querySelector(".bookmark-pill");
 
+function closeBookmark() {
+  bookmarkIcon.setAttribute("class", "material-icons-outlined nav-icon");
+  bookmarkPill.setAttribute("class", "nav-icon-container bookmark-pill");
+  bookmarkPopUp.style.display = "none";
+  html.style.height = "";
+  html.style.overflow = "";
+  body.style.height = "";
+  body.style.overflow = "";
+
+  bookmarkIcon.innerText = "bookmark_border";
+  //set home as active
+  homePill.setAttribute(
+    "class",
+    " nav-icon-container home-pill active-nav-icon"
+  );
+  homeIcon.setAttribute("class", "material-icons nav-icon");
+}
+
+// BOOKMARK
+
 function bookmarkToggle(e) {
-  bookmarkIcon.innerHTML = "bookmark";
+  openBookmark(e);
+  bookmarkIcon.innerText = "bookmark";
 
   //remove styling from others
   homePill.setAttribute("class", " nav-icon-container home-pill");
@@ -133,16 +166,96 @@ function homeToggle() {
   bookmarkIcon.innerHTML = "bookmark_outline";
 }
 
+//Bookmark list render
+
+const bookmarkListContainer = document.querySelector(
+  "#bookmark-list-container"
+);
+
+function bookmarkListRender(element) {
+  let newBookmarkListItem = document.createElement("div");
+  newBookmarkListItem.classList.add("bookmark-list-item");
+
+  let idNumber = document.createAttribute("number");
+  idNumber.value = element.getAttribute("number");
+  newBookmarkListItem.setAttribute("number", element.getAttribute("number"));
+
+  let newListImg = document.createElement("img");
+  newListImg.classList.add("list-img");
+  newListImg.setAttribute("src", element.getAttribute("image"));
+
+  let newListTtile = document.createElement("h2");
+  newListTtile.classList.add("list-title");
+  newListTtile.textContent = element.getAttribute("name");
+
+  let newListButton = document.createElement("span");
+  newListButton.classList.add("material-icons");
+  newListButton.classList.add("list-bookmark");
+  newListButton.setAttribute("number", element.getAttribute("number"));
+
+  newListButton.setAttribute("onclick", "favoriteToggle(this)");
+  newListButton.setAttribute("pressed", element.getAttribute("pressed"));
+
+  newListButton.textContent = "bookmark_add";
+
+  bookmarkListContainer
+    .appendChild(newBookmarkListItem)
+    .appendChild(newListImg);
+
+  newBookmarkListItem.appendChild(newListTtile);
+  newBookmarkListItem.appendChild(newListButton);
+}
+
+// Removes the empty bookmark icon if the user has bookmared sth
+const emptyBookmarkIcon = document.querySelector("#empty-bookmark");
+
+function emptyBookmark() {
+  if (
+    bookmarkListContainer.getElementsByClassName("bookmark-list-item").length >=
+    1
+  ) {
+    emptyBookmarkIcon.style.display = "none";
+  } else {
+    emptyBookmarkIcon.style.display = "block";
+  }
+}
+
+function resetBookmarkIcon(element) {
+  const resetIcon = document.querySelector(
+    `[number = "${element.getAttribute("number")}"]`
+  );
+  resetIcon.classList.replace("material-icons", "material-icons-outlined");
+  resetIcon.setAttribute("pressed", false);
+}
+
+function removeBookmarkItem(element) {
+  const removedElement = document.querySelector(
+    `[class = "bookmark-list-item"][number = "${element.getAttribute(
+      "number"
+    )}"]`
+  );
+  resetBookmarkIcon(element);
+  removedElement.remove();
+  console.log(removedElement);
+  console.log(element.getAttribute("pressed"));
+}
+
 //Favorite toggle
 
-let pressed = false;
 function favoriteToggle(x) {
-  if (pressed === false) {
+  favoritebutton(x);
+  emptyBookmark();
+}
+
+function favoritebutton(x) {
+  if (x.getAttribute("pressed") === "false") {
     x.classList.replace("material-icons-outlined", "material-icons");
-    pressed = true;
-  } else if (pressed === true) {
-    x.classList.replace("material-icons", "material-icons-outlined");
-    pressed = false;
+    x.setAttribute("pressed", true);
+    bookmarkListRender(x);
+    // resetBookmarkIcon(x);
+  } else if (x.getAttribute("pressed") === "true") {
+    removeBookmarkItem(x);
+    resetBookmarkIcon(x);
   }
 }
 
